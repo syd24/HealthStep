920a1cde573928927c88b585b4f364d8fc5864ae
YingdongShi <YingdongShi@2602845404@qq.com>
2017-06-16 11:24:18 +0800

    创建工程

---
+++ GetHealthStep/GetHealthStep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetHealthStep
+//
+//  Created by ADMIN on 17/6/16.
+//  Copyright © 2017年 ADMIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
